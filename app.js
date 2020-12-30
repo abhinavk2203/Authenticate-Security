70d6a8121431030f9d0c2d1099a398bb23a09fbd
Apple <apple@Apples-MacBook-Air.local>
2020-12-30 18:56:43 +0530

    Completed the Secret project

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({            //Schema for DB
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);       //this will plugin passLocalMongoose to the User schema
@@ -114,16 +115,48 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    }else{
-        res.redirect("/login");
-    }
+
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
 });
+
 
 app.get("/logout", function(req, res){
     req.logout();                       //passport method of logout
     res.redirect("/");
+});
+
+
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }else{
+        res.redirect("/login");
+    }    
+});
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 
 
